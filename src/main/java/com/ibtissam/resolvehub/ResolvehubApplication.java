1fd43c9b8888c6b7ff53037bddc8c61b2be10496
ibtissammassa <ibtissammassa16@gmail.com>
2026-08-31 12:09:30 +0100

    chore: initialize resolveHub spring boot app

---
+++ src/main/java/com/ibtissam/resolvehub/ResolvehubApplication.java
@@ -0,0 +1,13 @@
+package com.ibtissam.resolvehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolvehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResolvehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibtissam/resolvehub/ResolvehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibtissam.resolvehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResolvehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
